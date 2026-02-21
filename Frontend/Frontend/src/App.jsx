f3b9ff6ba9a8582e5a55ce0ceff1b23a41646ee5
Saksham9461 <sakshamojha9461@gmail.com>
2026-02-21 10:30:23 +0530

    page folder added

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <h1 className="text-4xl font-bold text-blue-800">
         Tailwind is Working 🚀
       </h1>
+      <LoginPage />
     </div>
   )
 }
